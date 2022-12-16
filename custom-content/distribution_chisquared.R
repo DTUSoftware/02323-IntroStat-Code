8e1d95b0c3a4cb0d0d2e0ddb37bc3e2f819b138f
HeroGamers <hero@herogamers.dev>
2022-12-16 22:11:37 +0100

    Chisquared change

--- custom-content/distribution_chisquared.R
+++ custom-content/distribution_chisquared.R
@@ -2,26 +2,35 @@
 #   CHISQUARE DISTRIBUTION
 # -----------------------------------------------------------------------------
 
-#  Insert value
+## 1D
+
+#  Insert values
 n <- 0
 s <- 0
 x <- 0
 alpha <- 0.01
 
-
 # Calculate values
-# 1d values
 df <- n - 1
 alpha_1 <- alpha / 2
 alpha_2 <- 1 - alpha_1
-# 2d
-n1 <- 3
-n2 <- 2
-alpha <- 0.01
-df2d <- (3 - 1) * (2 - 1)
-
-qchisq(1 - alpha, df2d)
 
 # Calculate Result
 sqrt(df * s^2 / qchisq(alpha_1, df))
 sqrt(df * s^2 / qchisq(alpha_2, df))
+
+
+## 2D
+
+#  Insert values
+n1 <- 3
+n2 <- 2
+alpha <- 0.01
+
+# Calculate values
+df2d <- (3 - 1) * (2 - 1)
+
+# Calculate Result
+qchisq(1 - alpha, df2d)
+
+
